4d0820b875f86cbee526b0f9eec20afff29b92ef
Jennifer Hsiao <etismeimet@gmail.com>
2021-01-13 20:57:11 -0800

    added code to generate subjobs.sh, added a few lines in job scripts to create log file

--- scripts/wflow.py
+++ scripts/wflow.py
@@ -3,7 +3,13 @@ import glob
 import argparse
 import numpy as np
 import pandas as pd
+import yaml
 
+from ideotype.data import DATA_PATH
+
+
+# TODO: really need to test this code
+# TODO: to properly do so, will need to setup test directory to test on
 # TODO: add code to mkdir for custom init files here as well
 # TODO: update some names to make code more readable
 # TODO: test that output directories and run/job files are correct
@@ -15,6 +21,14 @@ import pandas as pd
 a = argparse.ArgumentParser(
     description=" help text here ___ "
 )
+
+a.add_argument(
+    "exp_name",
+    type=str,
+    help="experiment name for simulations"
+)
+
+
 a.add_argument(
     "folder",
     type=str,
@@ -56,6 +70,20 @@ if not os.path.isdir(exp_dirct + folder):
         for var in cultivars:
             os.mkdir(exp_dirct + folder + folder_name +
                      str(year) + '/' + str(var))
+else:
+    raise ValueError(f'directory {exp_dirct + folder} already exists!')
+
+
+# step 0: fetch yaml file with experiment setup specs
+fname_param = os.path.join(DATA_PATH, 'inits', 'init_' + run_name + '.yml')
+
+if not os.path.isfile(fname_param):  # check whether init_.yml file exists
+    raise ValueError(f'init param file {fname_param} does not exist')
+
+with open(fname_param, 'r') as pfile:  # read in init param yaml file
+    dict_param = yaml.safe_load(pfile)
+
+dict_runspecs = dict_param['setup']
 
 # step 1: create run.txt files
 siteyears = pd.read_csv(exp_dirct + 'weadata/siteyears_filtered.csv',
@@ -78,8 +106,8 @@ standard_dict = {int(3): 'biology',
 
 
 for i in np.arange(siteyears.shape[0]):
-    site = siteyears.iloc[i,0]
-    year = str(siteyears.iloc[i,1])
+    site = siteyears.iloc[i, 0]
+    year = str(siteyears.iloc[i, 1])
 
     # setting up directories
     init_dirct_wea = exp_dirct + 'weadata/data/control/'
@@ -134,34 +162,77 @@ for i in np.arange(siteyears.shape[0]):
 
 
 # step 2: create job files that execute batch of run files
-dirct = exp_dirct + 'jobs/opt/'
 cultivars = glob.glob(exp_dirct + 'inits/var/*')
 treatment = 'cont'
 
-for i in np.arange(1961,2006):
-    for j in cultivars:
-        var = j.split('/')[-1].split('.')[-2]
+for year in np.arange(1961, 2006):
+    for cvar in cultivars:
+        var = cvar.split('/')[-1].split('.')[-2]
+        logfile = str(year) + '_' + str(var) + '.log'
         str1 = '#!/bin/bash\n'
         str2 = '#PBS -l nodes=1:ppn=1\n'
         str3 = '#PBS -l walltime=08:00:00\n'
         str4 = '#PBS -m a\n'
         str5 = '#PBS -M ach315@uw.edu\n'
         str6 = '#PBS -N ' + treatment + '_' + str(i) + '_' + str(var) + '\n'
-        str7 = '\n' 
-        str8 = 'FILES=' + full_dirct + str(i)+ '/' + str(var) + '/*\n'
-        str9 = '\n'
-        str10 = 'for file in $FILES\n'
-        str11 = 'do\n'
-        str12 = '    cd /home/disk/eos8/ach315/MAIZSIM\n'
-        str13 = '    timeout 15m maizsim $file\n'
-        str14 = 'done\n'
+        str7 = '\n'
+        str8 = 'FILES=' + full_dirct + str(year) + '/' + str(var) + '/*\n'
+        str9 = 'cd ' + os.path.join(dict_runspecs['path_sims'],
+                                    dict_runspecs['run_name'],
+                                    str(year),
+                                    str(cvar)) + '\n'
+        str10 = 'touch ' + logfile + '\n'
+        str11 = '\n'
+        str12 = 'for file in $FILES\n'
+        str13 = 'do\n'
+        str14 = '\tfname=$(echo $file)\n'  # grab file name
+        str15 = ('\tmaizsim_hash='
+                 '$(git describe --dirty --always --tags)\n')  # githash
+        str16 = f'\techo $fname,$maizsim_hash >> {logfile}\n'  # append to log
+        str17 = '\tcd /home/disk/eos8/ach315/MAIZSIM\n'
+        str18 = '\ttimeout 15m maizsim $file\n'
+        str19 = 'done\n'
 
-        strings = [str1, str2, str3, str4, str5, str6, str7, str8, str9, str10, str11, str12, str13, str14]
+        strings = [str1, str2, str3, str4, str5, str6,
+                   str7, str8, str9, str10, str11, str12,
+                   str13, str14, str15, str16, str17, str18, str19]
 
-        jobs = open(dirct + '/' + str(i) + '_' + str(var) + '.job', 'w')
+        jobs = open(os.path.join(dict_runspecs['path_jobs'],
+                                 str(year) + '_' + str(var) + '.job'), 'w')
         jobs.writelines(strings)
         jobs.close()
 
-# step 3: create bash script that automates qsub jobs
-# just wrote this directly thorugh terminal but probably want to code it up
 
+# step 3: create bash script that automates qsub jobs
+# TODO: still need to update this code
+dirct = os.path.join(dict_runspecs['path_jobs'], run)
+            # TODO: this last run doesn't make sense
+            # should be run name
+
+str1 = '#!/bin/bash\n'
+str2 = '\n'
+str3 = 'JOBS=' + dirct + '/*\n'
+str4 = '\n'
+str5 = 'for job in $JOBS\n'
+str6 = 'do\n'
+str7 = '\twhile [ `qstat | grep ach315 | wc -l` -ge 100 ]\n'
+str8 = '\tdo\n'
+str9 = '\tsleep 1\n'
+str10 = '\tdone\n'
+str11 = '\tqsub $job\n'
+str12 = 'done\n'
+
+strings = [str1, str2, str3, str4, str5, str6,
+           str7, str8, str9, str10, str11, str12]
+
+subjobs = open(os.join(dirct, 'subjosb.sh', 'w')) # TODO: rename subjobs.sh as 
+                                                  # subjobs_runame.sh
+subjobs.writelines(strings)
+subjobs.close()
+
+strings = [str1, str2, str3, str4, str5, str6, str7,
+           str8, str9, str10, str11, str12, str13, str14]
+
+jobs = open(dirct + '/' + str(i) + '_' + str(var) + '.job', 'w')
+jobs.writelines(strings)
+jobs.close()
