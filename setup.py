5d093911e3c51a4d7f1f36bec961ed6f2a3aa6f6
Jennifer Hsiao <etismeimet@gmail.com>
2021-01-13 21:26:34 -0800

    added some more description to package

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ from setuptools import setup
 setup_args = {
     "name": "ideotype",
     "author": "Jennifer Hsiao",
-    #"url": "https://github.com/RadioAstronomySoftwareGroup/pyradiosky",
-    #"license": "MIT",
-    #"description": "Python objects and interfaces for representing diffuse, extended and compact astrophysical radio sources",
-    #"long_description": readme,
-    #"long_description_content_type": "text/markdown",
+    "url": "https://github.com/jennhsiao/ideotype",
+    "license": "MIT",
+    "description": "maizsim ideotype project",
     "package_dir": {"ideotype": "ideotype"},
     "packages": ["ideotype"],
     "scripts": glob.glob("scripts/*"),
     "use_scm_version": True,
     "include_package_data": True,
-    "install_requires": ["pandas", "pyyaml", "setuptools_scm"]  # need to add more requires here
+    "install_requires": ["pandas", "pyyaml", "setuptools_scm"]
+    # TODO: add more install_requires here
 }
 
 if __name__ == "__main__":
