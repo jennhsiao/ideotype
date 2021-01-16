cce2e3cde4d2ae9e6f3a3979fcd50877138dbcfb
Jennifer Hsiao <etismeimet@gmail.com>
2021-01-15 17:42:18 -0800

    setup utils.py file - ready to move some small functions from func.py here

---
+++ ideotype/utils.py
@@ -0,0 +1,4 @@
+# TODO: move relevant functions from func.py into here
+# TODO: some functions in func.py should be taken out and
+# turned into their own modules or incorporated into other scripts
+# only small functions that serve some utility function should be placed here
