5ff57005f3f6ce87be04144ad1b1e8977e0fdc9e
Jennifer Hsiao <etismeimet@gmail.com>
2021-01-20 16:01:46 -0800

    notes on namespace

--- ideotype/__init__.py
+++ ideotype/__init__.py
@@ -1,8 +1,21 @@
 from setuptools_scm import get_version
 from pathlib import Path
 from pkg_resources import get_distribution, DistributionNotFound
-from .read_sims import *
 
+from .read_sims import *
+from .utils import *
+from .sql_declarative import *
+#from .sql_insert import *
+#from . import sql_declarative
+#from . import sql_insert
+
+# from .sql_insert import * vs. from . import sql_insert
+# are essentially the same thing but results in different namespace 
+# option 1 (from .sql_insert import *):
+#   you can access functions in modules by
+#   directly calling the function name
+#   e.g. from ideotype import CC_VPD
+#   instead of from from ideotype.utils import CC_VPD
 
 try:
     # get accurate version for developer installs
