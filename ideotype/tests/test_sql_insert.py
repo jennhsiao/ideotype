4bd0622ed6e44cb1d2ba060cdc0944977b781df1
Jennifer Hsiao <etismeimet@gmail.com>
2021-01-28 17:47:44 -0800

    updated wea, sims, and params file paths that now point to test_data in repo

--- ideotype/tests/test_sql_insert.py
+++ ideotype/tests/test_sql_insert.py
@@ -1,23 +1,23 @@
 """Testing DB insert values."""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from numpy import genfromtxt
 
-from ideotype import WeaData, Sims, Params, IdeotypeBase
-
+from ideotype import WeaData, Sims, Params
 
 # point to test files
-fpath_wea = '/home/disk/eos8/ach315/upscale/db/test_data/wea/727810_2005.txt'
-fpath_sim = ('/home/disk/eos8/ach315/upscale/db/test_data/sim/opt/var_15/'
-             'out1_727810_2005_var_15.txt')
-fpath_param = '/home/disk/eos8/ach315/upscale/db/test_data/param/param_opt.csv'
+fpath_wea = ('/home/disk/eos8/ach315/ideotype/ideotype/data'
+             '/test_data/wea/725300_1964.txt')
+fpath_sim = ('/home/disk/eos8/ach315/ideotype/ideotype/data'
+             '/test_data/sims/test/1964/var_6/out1_725300_1964_var_6.txt')
+# TODO: update
+fpath_param = ('/home/disk/eos8/ach315/ideotype/ideotype/data'
+               '/test_data/params/param_test.csv')
 
 
 def test_insert_weadata(ideotype_session):
     """Test if query output matches weather data."""
     query = ideotype_session.query(WeaData).filter(
-        WeaData.site == '727810').filter(
-            WeaData.year == 2005).order_by(
+        WeaData.site == '725300').filter(
+            WeaData.year == 1964).order_by(
                 WeaData.date, WeaData.time).limit(10)
     results = query.all()
 
@@ -30,7 +30,7 @@ def test_insert_weadata(ideotype_session):
 
     for index, row in enumerate(data):
         obj_wea = results[index]
-        assert obj_wea.site == '727810'
+        assert obj_wea.site == '725300'
         assert obj_wea.jday == row[0]
         assert obj_wea.date.year == int(row[1].split('/')[-1].split("'")[0])
         assert obj_wea.time == row[2]
@@ -39,10 +39,11 @@ def test_insert_weadata(ideotype_session):
 def test_insert_sims(ideotype_session):
     """Test if query output match sims."""
     query = ideotype_session.query(Sims).filter(
-        Sims.site == '727810').filter(
-            Sims.year == 2005).filter(
-                Sims.cvar == 15).limit(10)
+        Sims.site == '725300').filter(
+            Sims.year == 1964).filter(
+                Sims.cvar == 6).limit(10)
     results = query.all()
+    assert len(results) == 10
 
     data = genfromtxt(fpath_sim,
                       delimiter=',',
@@ -52,7 +53,7 @@ def test_insert_sims(ideotype_session):
 
     for index, row in enumerate(data):
         obj_sim = results[index]
-        assert obj_sim.site == '727810'
+        assert obj_sim.site == '725300'
         assert obj_sim.year == int(row[0].split('/')[-1])
         assert obj_sim.DM_ear == row[40]
         assert obj_sim.pheno == row[-1].strip()
