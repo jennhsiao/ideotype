7b8e4315f5ad8ea034fd3c26d1dbe52b518a23f8
Jennifer Hsiao <etismeimet@gmail.com>
2021-01-29 19:13:16 -0800

    changes to speed up insert speed

--- ideotype/sql_insert.py
+++ ideotype/sql_insert.py
@@ -15,6 +15,12 @@ Tables include:
 * linked primay keys.
 
 """
+import time
+import atexit
+
+import numpy as np
+import pandas as pd
+import line_profiler
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from numpy import genfromtxt
@@ -27,6 +33,9 @@ from ideotype.sql_declarative import (
 #                             NASSYield, SoilClass)
 
 from ideotype.utils import get_filelist, CC_VPD
+
+profile = line_profiler.LineProfiler()
+atexit.register(profile.print_stats)
 
 
 def insert_siteinfo(fpath_siteinfo, fpath_db, session=None):
@@ -129,6 +138,27 @@ def insert_params(fpath_params, fpath_db, run_name, session=None):
     session.commit()
 
 
+def _time_estimate(time_list, count_list, nfiles, fname):
+    """
+    """
+    time_perloop = (time_list[-1] - time_list[0])/count_list[-1]
+    count = count_list[-1]
+    estimated_time = (nfiles - count)/time_perloop
+    unit = 's'
+    if estimated_time > 60:
+        estimated_time = estimated_time/60
+        unit = 'm'
+    if estimated_time > 60:
+        estimated_time = estimated_time/60
+        unit = 'h'
+
+    print(f'on file {count+1} of {nfiles}, '
+          f'filename: {fname}, '
+          f'time per loop = {time_perloop} s, '
+          f'estimated time remaining {estimated_time} {unit}')
+
+
+#@profile
 def insert_weadata(dirct_weadata, fpath_db, session=None):
     """
     Propagate values to DB table - WeaData.
@@ -145,6 +175,7 @@ def insert_weadata(dirct_weadata, fpath_db, session=None):
         Database session, default to None and generates new session.
 
     """
+    print('importing weafiles')
     if session is None:
         engine = create_engine('sqlite:///' + fpath_db)
         IdeotypeBase.metadata.bind = engine
@@ -153,7 +184,21 @@ def insert_weadata(dirct_weadata, fpath_db, session=None):
 
     weafiles = get_filelist(dirct_weadata)
 
-    for weafile in weafiles:
+    timelist = [time.perf_counter()]
+    count_times = [0]
+    nfiles = len(weafiles)
+    if nfiles > 10:
+        printreport_loop = (nfiles*(
+            (np.arange(10)+1)/10)).round().astype(int).tolist()
+    else:
+        printreport_loop = np.arange(nfiles - 1) + 1
+
+    for count, weafile in enumerate(weafiles):
+        if count == 1 or count in printreport_loop:
+            timelist.append(time.perf_counter())
+            count_times.append(count)
+            _time_estimate(timelist, count_times, nfiles, weafile)
+
         site_id = weafile.split('/')[-1].split('_')[0]
         year_id = int(weafile.split('/')[-1].split('_')[1].split('.')[0])
         data = genfromtxt(weafile,
@@ -162,6 +207,7 @@ def insert_weadata(dirct_weadata, fpath_db, session=None):
                           dtype=(int, 'U12', int, float,
                                  float, float, float, int))
 
+        object_list = []
         for row in data:
             # make an object instance
             record = WeaData(
@@ -178,13 +224,16 @@ def insert_weadata(dirct_weadata, fpath_db, session=None):
                 vpd=round(CC_VPD(row[4], row[6]/100), 2))
 
             # add row data to record
-            session.add(record)
+            #session.add(record)
+            object_list.append(record)
 
+        session.bulk_save_objects(object_list)
         # add data to database
         session.commit()
 
 
-def insert_sims(dirct_sims, fpath_db, run_name, session=None):
+#@profile
+def insert_sims(dirct_sims, fpath_db, run_name, session=None, core=True):
     """
     Propagate values to DB table - Sims.
 
@@ -200,6 +249,8 @@ def insert_sims(dirct_sims, fpath_db, run_name, session=None):
         Database session, default to None and generates new session.
 
     """
+    print('importing sims')
+    start_time = time.perf_counter()
     if session is None:
         engine = create_engine('sqlite:///' + fpath_db)
         IdeotypeBase.metadata.bind = engine
@@ -209,77 +260,109 @@ def insert_sims(dirct_sims, fpath_db, run_name, session=None):
     simfiles = get_filelist(dirct_sims)
     runame = run_name
 
-    for sim in simfiles:
+    timelist = [time.perf_counter()]
+    count_times = [0]
+    nfiles = len(simfiles)
+
+    if nfiles > 10:
+        printreport_loop = (nfiles*(
+            (np.arange(10)+1)/10)).round().astype(int).tolist()
+    else:
+        printreport_loop = np.arange(nfiles-1) + 1
+
+    object_list = []
+    core_list = []
+
+    for count, sim in enumerate(simfiles):
+        if count == 1 or count in printreport_loop:
+            timelist.append(time.perf_counter())
+            count_times.append(count)
+            _time_estimate(timelist, count_times, nfiles, sim)
+
         # parse info needed from file name
         site_id = str(sim.split('/')[-1].split('.')[0].split('_')[1])
         year_id = int(sim.split('/')[-1].split('.')[0].split('_')[2])
         cvar_id = int(sim.split('/')[-1].split('.')[0].split('_')[4])
 
         # read in individual sim file
-        data = genfromtxt(
-            sim,
-            delimiter=',',
-            skip_header=1,
-            dtype=tuple(['U10'] + [float]*48 + ['<U50']),
-            )
+#        data = genfromtxt(
+#            sim,
+#            delimiter=',',
+#            skip_header=1,
+#            dtype=tuple(['U10'] + [float]*48 + ['<U50']),
+#            )
+
+        # pandas read_csv actually faster than genfromtxt
+        data = pd.read_csv(sim, delimiter=',')
+        data = data.to_records(index=False)
 
         # read in each line of sim file
+        #object_list = []
         for row in data:
-            record = Sims(
-                run_name=runame,
-                site=site_id,
-                year=year_id,
-                cvar=cvar_id,
-                date=datetime.strptime(row[0], '%m/%d/%Y'),
-                time=row[2],
-                leaves=row[3],
-                leaves_mature=row[4],
-                leaves_dropped=row[5],
-                LA_perplant=row[6],
-                LA_dead=row[7],
-                LAI=row[8],
-                leaf_wp=row[10],
-                temp_soil=row[13],
-                temp_air=row[14],
-                temp_canopy=row[15],
-                ET_dmd=row[16],
-                ET_sply=row[17],
-                Pn=row[18],
-                Pg=row[19],
-                resp=row[20],
-                av_gs=row[21],
-                LAI_sun=row[22],
-                LAI_shade=row[23],
-                PFD_sun=row[24],
-                PFD_shade=row[25],
-                An_sun=row[26],
-                An_shade=row[27],
-                Ag_sun=row[28],
-                Ag_shade=row[29],
-                gs_sun=row[30],
-                gs_shade=row[31],
-                VPD=row[32],
-                Nitr=row[33],
-                N_Dem=row[34],
-                NUpt=row[35],
-                LeafN=row[36],
-                PCRL=row[37],
-                DM_total=row[38],
-                DM_shoot=row[39],
-                DM_ear=row[40],
-                DM_leaf=row[41],
-                DM_stem=row[43],
-                DM_root=row[44],
-                AvailW=row[47],
-                solubleC=row[48],
-                pheno=row[49].strip()  # remove leading whitespace in output
+            core_list.append(
+                {
+                    'run_name': runame,
+                    'site': site_id,
+                    'year': year_id,
+                    'cvar': cvar_id,
+                    'jday': row[1],
+                    'date': row[0],
+                    'time': row[2],
+                    'leaves': row[3],
+                    'leaves_mature': row[4],
+                    'leaves_dropped': row[5],
+                    'LA_perplant': row[6],
+                    'LA_dead': row[7],
+                    'LAI': row[8],
+                    'leaf_wp': row[10],
+                    'temp_soil': row[13],
+                    'temp_air': row[14],
+                    'temp_canopy': row[15],
+                    'ET_dmd': row[16],
+                    'ET_sply': row[17],
+                    'Pn': row[18],
+                    'Pg': row[19],
+                    'resp': row[20],
+                    'av_gs': row[21],
+                    'LAI_sun': row[22],
+                    'LAI_shade': row[23],
+                    'PFD_sun': row[24],
+                    'PFD_shade': row[25],
+                    'An_sun': row[26],
+                    'An_shade': row[27],
+                    'Ag_sun': row[28],
+                    'Ag_shade': row[29],
+                    'gs_sun': row[30],
+                    'gs_shade': row[31],
+                    'VPD': row[32],
+                    'Nitr': row[33],
+                    'N_Dem': row[34],
+                    'NUpt': row[35],
+                    'LeafN': row[36],
+                    'PCRL': row[37],
+                    'DM_total': row[38],
+                    'DM_shoot': row[39],
+                    'DM_ear': row[40],
+                    'DM_leaf': row[41],
+                    'DM_stem': row[43],
+                    'DM_root': row[44],
+                    'AvailW': row[47],
+                    'solubleC': row[48],
+                    'pheno': row[49].strip()
+                }
+
             )
 
-            # add row data to record
-            session.add(record)
+    engine.execute(
+        Sims.__table__.insert(),
+        core_list
+    )
 
-        # commit data to DB
-        session.commit()
+    # commit data to DB
+    session.commit()
+
+    end_time = time.perf_counter()
+    print(f'sims total run time: {end_time - start_time} s')
 
 
 def insert_loginit(fpath_log, fpath_db, session=None):
