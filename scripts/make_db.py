76679a44defee083743edc9eb2dc8fbf16a82473
Jennifer Hsiao <etismeimet@gmail.com>
2021-03-05 16:27:54 -0800

    remove some commented out old code

--- scripts/make_db.py
+++ scripts/make_db.py
@@ -41,20 +41,14 @@ else:
 init_dict = read_inityaml(args.run_name)
 
 # setup pointers to db table files
-# TODO: do I want these paths directly from yaml file?
 fpath_siteinfo = init_dict['site_summary']
 fpath_params = os.path.join(init_dict['path_params'])
 dirct_weadata = init_dict['path_wea']
 dirct_sims = os.path.join(init_dict['path_sims'])
 
-# fpath_siteinfo = '/home/disk/eos8/ach315/upscale/weadata/site_summary.csv'
-# fpath_params = '/home/disk/eos8/ach315/upscale/params/param_opt.csv'
-# dirct_weadata = '/home/disk/eos8/ach315/upscale/weadata/data/control/'
-# dirct_sims = '/home/disk/eos8/ach315/upscale/sims/opt/'
-
 # insert values into table for specific run
 # TODO: is there a way to raise some error if tables have values already?
-# TODO: this would signal that instead of make_db I'd want to update_db?
+# TODO: signal that instead of make_db I'd want to update_db instead.
 insert_siteinfo(fpath_siteinfo, fpath_db)
 insert_params(fpath_params, fpath_db, args.run_name)
 insert_weadata(dirct_weadata, fpath_db)
