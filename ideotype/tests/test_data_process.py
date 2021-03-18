60291f7ffcfdde47cb4553e477dc5142edf58608
Jennifer Hsiao <etismeimet@gmail.com>
2021-03-18 11:36:02 -0700

    address issue with sims filesize too large for GitHub causing testing issues

--- ideotype/tests/test_data_process.py
+++ ideotype/tests/test_data_process.py
@@ -12,8 +12,22 @@ def test_read_data():
 
 
 def test_agg_sims():
-    """Test agg_sims."""
-    yamlfile = os.path.join(DATA_PATH, 'files', 'filepaths.yml')
+    """
+    Test agg_sims.
+
+    Two testing files to address GitHub file size limit.
+    1. Testing on server works with all simulation outputs.
+    2. Testing on GitHub repo works with only a portion of the sim outputs.
+
+    """
+    if os.path.expanduser('~/') == '/home/disk/eos8/ach315/':
+        filepaths_yaml = 'filepaths_local.yml'
+        aggshape = (100, 129)
+    else:
+        filepaths_yaml = 'filepaths_repo.yml'
+        aggshape = (100, 111)
+
+    yamlfile = os.path.join(DATA_PATH, 'files', filepaths_yaml)
 
     (df_sims, df_sites, df_wea,
      df_params, df_all, df_matured) = read_data(yamlfile)
@@ -23,12 +37,12 @@ def test_agg_sims():
     # mean
     how = 'mean'
     mx_mean = agg_sims(df, groups, how)
-    assert mx_mean.shape == (100, 129)
+    assert mx_mean.shape == aggshape
 
     how = 'variance'
     mx_variance = agg_sims(df, groups, how)
-    assert mx_variance.shape == (100, 129)
+    assert mx_variance.shape == aggshape
 
     how = 'std'
     mx_std = agg_sims(df, groups, how)
-    assert mx_std.shape == (100, 129)
+    assert mx_std.shape == aggshape
