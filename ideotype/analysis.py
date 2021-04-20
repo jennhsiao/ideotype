ab144bc6da37d081cb5198429be7c96039e819a6
Jennifer Hsiao <etismeimet@gmail.com>
2021-04-19 19:21:56 -0700

    add code to read in param.csv file

--- ideotype/analysis.py
+++ ideotype/analysis.py
@@ -1,26 +1,42 @@
 """Compilation of functions for analyzing maizsim output."""
 
+import os
+
 import pandas as pd
 import numpy as np
+from numpy import genfromtxt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from SALib.analyze import rbd_fast
 
+from ideotype.init_params import params_sample
 
-def run_rbdfast(problem, X, Y):
+
+def run_rbdfast(N_sample, run_name):
     """
     Sensitivity analysis through RBD-FAST method.
 
     Parameters
     ----------
-    problem : dict
-        SALib parameter problem definition.
-    X : np.matrix
-        parameter inputs.
-    Y : np.matrix
-        model outputs.
+    N_sample : int
+        number of samples to generate.
+    run_name : str
+        run name for batch sims.
 
     """
+    problem, param_values = params_sample(N_sample)
+
+    # * param_values cannot directly be used for rbd_fast analysis here
+    # since values change with each sampling
+    # read in previously saved param.csv file as np.matrix
+    fpath_read = os.path.join(os.path.expanduser('~'),
+                              'upscale', 'params',
+                              f'param_{run_name}.csv'
+                              )
+    X = genfromtxt(fpath_read, delimiter=',', skip_header=1)
+
+    # TODO: still need code that reads in Y here
+
     Si = rbd_fast.analyze(problem, X, Y, print_to_consol=False)
 
     return Si
