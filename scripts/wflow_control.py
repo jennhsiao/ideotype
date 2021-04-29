50ee01c064ead4ec840c509d9be96d78e149dfb5
Jennifer Hsiao <etismeimet@gmail.com>
2021-04-29 16:43:19 -0700

    cont_cvars=False for make_inits

--- scripts/wflow_control.py
+++ scripts/wflow_control.py
@@ -6,7 +6,7 @@ from ideotype.wflow_setup import (make_dircts, make_inits, make_cultivars,
 
 log_fetchinfo('control')
 make_dircts('control', cont_cvars=False)
-make_inits('control')
+make_inits('control', cont_cvars=False)
 make_cultivars('control', cont_cvars=False)
 make_runs('control', cont_cvars=False)
 make_jobs('control', cont_cvars=False)
