d25dd9717b9579f7726bb17cd40f3d21a633d504
Jennifer Hsiao <etismeimet@gmail.com>
2021-05-25 16:30:44 -0700

    update values to match manuscript

--- scripts/process_wea.py
+++ scripts/process_wea.py
@@ -4,7 +4,8 @@ from ideotype.weafile_process import (read_wea,
                                       wea_combine,
                                       wea_preprocess,
                                       wea_siteyears,
-                                      wea_filter)
+                                      wea_filter,
+                                      make_weafile)
 
 # Read in raw weather data file
 read_wea(1961, 2010)
@@ -18,12 +19,19 @@ wea_combine(basepath)
 df_temp, df_rh, df_precip, df_solrad = wea_preprocess(basepath)
 
 # Select valid site-years
+gseason_start = 3
+gseason_end = 11
 crthr = 2
-siteyears = wea_siteyears(df_temp, df_precip, df_solrad, crthr)
+siteyears = wea_siteyears(df_temp, df_rh, df_precip, df_solrad, 
+                          gseason_start, gseason_end, crthr)
 
 # Filter site-years based on area, irrigation, & estimated pdate
 area = 10000
-irri = 50
-siteyears_filtered = wea_filter(siteyears, area, irri)
+irri = 30
+yearspersite = 15
+siteyears_filtered = wea_filter(siteyears, area, irri, yearspersite)
 
 # Convert met data into individual maizsim weather data files
+outpath = '/home/disk/eos8/ach315/upscale/weadata/data/test/'
+make_weafile(
+    siteyears_filtered, df_temp, df_rh, df_precip, df_solrad, outpath)
