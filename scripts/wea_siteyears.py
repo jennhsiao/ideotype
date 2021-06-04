2e2ce0fb7ff1ebc65f8b065bc9b672f171e1f3fc
Jennifer Hsiao <etismeimet@gmail.com>
2021-06-03 23:08:21 -0700

    write out wea_summary in this script as well

--- scripts/wea_siteyears.py
+++ scripts/wea_siteyears.py
@@ -3,7 +3,8 @@
 import os
 from ideotype.weafile_process import (wea_preprocess,
                                       wea_siteyears,
-                                      wea_filter)
+                                      wea_filter,
+                                      wea_summarize)
 
 # Run name
 run_name = 'control_fixpd'
@@ -29,3 +30,13 @@ yearspersite = 15
 siteyears_filtered = wea_filter(siteyears, area, irri, yearspersite)
 siteyears_filtered.to_csv(
     os.path.join(outpath, f'siteyears_{run_name}.csv'), index=False)
+
+# Summarize site-year weather info
+gseason_start_weasummary = 3
+gseason_end_weasummary = 10
+
+df_wea = wea_summarize(siteyears_filtered,
+                       df_temp, df_rh, df_precip, df_solrad,
+                       gseason_start_weasummary, gseason_end_weasummary)
+df_wea.to_csv(
+    os.path.join(outpath, f'wea_summary_{run_name}.csv'), index=False)
