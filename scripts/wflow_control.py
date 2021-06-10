cd0137f44272aa485679f777b54de41aa4f678c7
Jennifer Hsiao <etismeimet@gmail.com>
2021-06-09 18:49:20 -0700

    made code more generic to accomodate different controls

--- scripts/wflow_control.py
+++ scripts/wflow_control.py
@@ -1,13 +1,35 @@
-"""Generate init text files for CONTROL maizsim simulations."""
+"""
+Generate init text files for CONTROL maizsim simulations.
+
+Two control sims
+- control_fixpd: fixed planting date
+- control_dympd: dynamice planting date
+
+"""
+import argparse
 
 from ideotype.log import log_fetchinfo
 from ideotype.wflow_setup import (make_dircts, make_inits, make_cultivars,
                                   make_runs, make_jobs, make_subjobs)
 
-log_fetchinfo('control')
-make_dircts('control', cont_cvars=False)
-make_inits('control', cont_cvars=False)
-make_cultivars('control', cont_cvars=False)
-make_runs('control', cont_cvars=False)
-make_jobs('control', cont_cvars=False)
-make_subjobs('control')
+# Setup argparse for terminal control
+a = argparse.ArgumentParser(
+    description='additional info to setup project directories and files'
+)
+
+a.add_argument(
+    'run_name',
+    type=str,
+    help='run name for batch of simulation experiments')
+
+# Fetch args
+args = a.parse_args()
+
+# Workflow
+log_fetchinfo(args.run_name)
+make_dircts(args.run_name, cont_cvars=False)
+make_inits(args.run_name, cont_cvars=False)
+make_cultivars(args.run_name, cont_cvars=False)
+make_runs(args.run_name, cont_cvars=False)
+make_jobs(args.run_name, cont_cvars=False)
+make_subjobs(args.run_name)
