fb322a3db3686651fa1e7eb3ee8fc0b4a3ea3257
Jennifer Hsiao <etismeimet@gmail.com>
2021-07-19 11:49:54 -0700

    query pheno info

--- ideotype/sql_query.py
+++ ideotype/sql_query.py
@@ -1,7 +1,11 @@
 """Query DB for analyses."""
+import csv
+from re import M
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import func
+from sqlalchemy import func, and_
+from sqlalchemy.sql.expression import distinct
 
 from ideotype.sql_declarative import (
     IdeotypeBase, WeaData, Sims, Params,
@@ -16,9 +20,6 @@ def query_weadata(fpath_db):
     - Variance of meteorology at each site.
 
     """
-    # TODO: figure out how to query mean total rainfall across years per site
-    # TODO: calculate variance?
-
     engine = create_engine('sqlite:///' + fpath_db)
     IdeotypeBase.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
@@ -32,10 +33,27 @@ def query_weadata(fpath_db):
                           ).group_by(WeaData.site)
     results = query.all()
 
+    #  query output as csv
+    columns = []
+    for item in query.column_descriptions:
+        columns.append(item['name'])
+
+    with open('testoutput.csv', 'w') as outfile:
+        outcsv = csv.writer(outfile)
+        outcsv.writerow(columns)
+        for row in results:
+            outcsv.writerow(row)
 
 
 def query_sims(fpath_db):
     """
+    Sims query.
+
+    - Final yield for each site-year-cvar combination.
+    - Yield variation across cvars.
+    - Yield variation across sites.
+    - Yield variation across years.
+
     """
     engine = create_engine('sqlite:///' + fpath_db)
     IdeotypeBase.metadata.bind = engine
@@ -43,9 +61,61 @@ def query_sims(fpath_db):
     session = DBSession()
 
     query = session.query(Sims.site.label('site'),
+                          Sims.year.label('year'),
                           Sims.cvar.label('cavr'),
                           Sims.DM_ear.label('DM_ear'),
                           Sims.pheno.label('pheno')
                           ).group_by(Sims.site, Sims.cvar).filter(
-                              Sims.pheno == '"Matured"'
-                          )
+                              Sims.pheno == '"Matured"')
+    results = query.all()
+
+    return(results)
+
+#    query = session.query(Sims.pheno).group_by(Sims.pheno).distinct()
+
+#    query = session.query(Sims.site.label('site'),
+#                          Sims.cvar.label('cavr'),
+#                          func.avg(Sims.DM_ear).label('mean_DM_ear'),
+#                          func.stdev(Sims.DM_ear).label('stdev_DM_ear'),
+#                          Sims.pheno.label('pheno')
+#                          ).group_by(Sims.site, Sims.cvar).filter(
+#                              Sims.pheno == '"grainFill"')
+
+
+def query_pheno(fpath_db, phenos):
+    """
+    Query pheno info.
+
+    Parameters
+    ----------
+    fpath_db : str
+    phenos : list
+        List of top phenotype numbers.
+
+    Returns
+    -------
+    query : sqlalchemy query
+    results : list
+        List of query results.
+
+    """
+    engine = create_engine('sqlite:///' + fpath_db)
+    IdeotypeBase.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    query = session.query(Sims.cvar.label('cvar'),
+                          Sims.site.label('site'),
+                          Sims.year.label('year'),
+                          Sims.pheno.label('pheno'),
+                          func.count(distinct(Sims.jday)).label('pheno_days')
+                          ).group_by(
+                              Sims.cvar, Sims.site, Sims.year, Sims.pheno
+                          ).filter(
+                              and_(
+                                  Sims.cvar.in_(phenos)
+                              ))
+
+    results = query.all()
+
+    return(query, results)
