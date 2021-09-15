af0c440a79cf5f1a56db6c6c53f8174fdfb89377
Jennifer Hsiao <etismeimet@gmail.com>
2021-09-15 15:58:23 -0700

    add print statements to better track progress

--- scripts/wflow.py
+++ scripts/wflow.py
@@ -34,23 +34,30 @@ a.add_argument(
 args = a.parse_args()
 
 # SETP 0.0: document log file
+print('*** step 0.0: document log file')
 log_fetchinfo(args.run_name)
 
 # STEP 0.1: setup directories
+print('*** step 0.1: set up directories')
 make_dircts(args.run_name)
 
 # STEP 1: create maizsim initial files
 # init.txt, time.txt, climate.txt, management.txt
+print('*** step 1: make inits')
 make_inits(args.run_name)
 
 # STEP 2: create cultivar files
+print('*** step 2: make cultivar files')
 make_cultivars(args.run_name)
 
 # STEP 3: create run.txt files
+print('*** step 3: make run files')
 make_runs(args.run_name)
 
 # step 4: create job files that execute batch of run files
+print('*** step 4: make job files')
 make_jobs(args.run_name)
 
 # step 5: create bash script that automates qsub jobs
+print('*** step 5: make subjob file')
 make_subjobs(args.run_name)
