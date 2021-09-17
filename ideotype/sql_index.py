c87021c42e82afcbe5766c6945c034037cd032aa
Jennifer Hsiao <etismeimet@gmail.com>
2021-09-17 14:47:03 -0700

    add print statements

--- ideotype/sql_index.py
+++ ideotype/sql_index.py
@@ -15,16 +15,25 @@ def make_index(fpath_db):
     engine = create_engine('sqlite:///' + fpath_db)
 
     id_year = Index('id_year', Sims.year)
+    print('>> making id_year')
     id_year.create(engine)
 
     id_site = Index('id_site', Sims.site)
+    print('>> making id_site')
     id_site.create(engine)
 
     id_runame = Index('id_runame', Sims.run_name)
+    print('>> making id_runame')
     id_runame.create(engine)
 
     id_jday = Index('id_jday', Sims.jday)
+    print('>> making id_jday')
+    id_jday.create(engine)
+
+    id_jday = Index('id_time', Sims.time)
+    print('>> making id_time')
     id_jday.create(engine)
 
     id_pheno = Index('id_pheno', Sims.pheno)
+    print('>> making id_pheno')
     id_pheno.create(engine)
